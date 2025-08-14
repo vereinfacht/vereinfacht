fa277d8908e1cee4b7eedd6eaa2a6771778303cf
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2025-08-14 10:55:58 +0200

    fix: remove unnecessary required prop

--- web_application/src/app/components/MultiselectInput/MultiselectInput.tsx
+++ web_application/src/app/components/MultiselectInput/MultiselectInput.tsx
@@ -109,7 +109,6 @@ export default function MultiselectInput({
                                           )
                                         : ''
                                 }
-                                required
                             />
                         )}
                     </>
