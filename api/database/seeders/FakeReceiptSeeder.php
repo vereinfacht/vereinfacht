14fdf64a05d548a11405c6b74608eefb9d562735
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2025-08-26 13:27:58 +0200

    fix: update receipt seeder

--- api/database/seeders/FakeReceiptSeeder.php
+++ api/database/seeders/FakeReceiptSeeder.php
@@ -22,15 +22,18 @@ class FakeReceiptSeeder extends Seeder
                 ]);
 
             $transactions = Transaction::where('club_id', $club->id)->get();
-            $noTransactionReceipts = $receipts->random(max(1, floor($receipts->count() * 0.3)));
 
-            $receipts->each(function ($receipt) use ($transactions, $noTransactionReceipts) {
+            $noTransactionReceipts = $receipts->random(max(1, floor($receipts->count() * 0.3)));
+            $unusedTransactions = $transactions->random(max(1, floor($transactions->count() * 0.2)));
+            $usableTransactions = $transactions->diff($unusedTransactions);
+
+            $receipts->each(function ($receipt) use ($usableTransactions, $noTransactionReceipts) {
                 if ($noTransactionReceipts->contains($receipt)) {
                     return;
                 }
 
                 $receipt->transactions()->attach(
-                    $transactions->random(rand(1, 3))->pluck('id')->toArray()
+                    $usableTransactions->random(rand(1, 3))->pluck('id')->toArray()
                 );
             });
         });
