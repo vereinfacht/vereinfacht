44ec52f28bcbdadf8c9cb6c1bf9639310d5ac665
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2025-08-27 13:25:28 +0200

    fix: remove unnecessary curly braces

--- web_application/src/app/[lang]/admin/(secure)/finances/receipts/_components/receipts-table.tsx
+++ web_application/src/app/[lang]/admin/(secure)/finances/receipts/_components/receipts-table.tsx
@@ -114,7 +114,7 @@ export default function ReceiptsTable({
                                 </>
                             ) : (
                                 <>
-                                    {<CircleUserRound />}{' '}
+                                    <CircleUserRound />
                                     {financeContact.fullName ?? ''}
                                 </>
                             )
