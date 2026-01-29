9cc82182d2006c01c989faf1cce655ab21f68343
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2026-01-29 11:48:05 +0100

    chore: remove media policy mapping from AuthServiceProvider

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -16,7 +16,6 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         \Spatie\Permission\Models\Role::class => \App\Policies\RolePolicy::class,
         \Spatie\Permission\Models\Permission::class => \App\Policies\PermissionPolicy::class,
-        \App\Models\Media::class => \App\Policies\MediaPolicy::class,
     ];
 
     /**
