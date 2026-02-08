e46b204068c0d57e69e64d2f59f1623cac02f7fb
Lars Krause <lars.krause@visuellverstehen.de>
2026-02-08 16:54:27 +0100

    fix: build error

--- web_application/src/app/[lang]/admin/(secure)/finances/contacts/_components/contacts-table.tsx
+++ web_application/src/app/[lang]/admin/(secure)/finances/contacts/_components/contacts-table.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { financeContactTypeOptions } from '@/actions/financeContacts/list.schema';
-import CreateButton from '@/app/[lang]/admin/(secure)/components/CreateButton';
 import { DataTable } from '@/app/components/Table/DataTable';
 import { HeaderOptionFilter } from '@/app/components/Table/HeaderOptionFilter';
 import HeaderSort from '@/app/components/Table/HeaderSort';
