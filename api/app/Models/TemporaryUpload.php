f0995d14faae6a078cc7a789cf57001411ab5f4f
Yamen Hadla <yamen.hadla@visuellverstehen.de>
2026-02-09 15:29:53 +0100

    feat: switch from scheduled to immediate media preview generation, refs #167

--- api/app/Models/TemporaryUpload.php
+++ api/app/Models/TemporaryUpload.php
@@ -2,16 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use App\Models\Traits\HasPreviewConversions;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class TemporaryUpload extends Model implements HasMedia
 {
     use InteractsWithMedia;
+    use HasPreviewConversions;
 
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('temporary');
+    }
+
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->registerPreviewConversion($media);
     }
 }
