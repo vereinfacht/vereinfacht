2a45e1e2ba48dcbec3346bd43a7a64bfa3ca98c0
Fatemeh Amani <fatemeh.amani@visuellverstehen.de>
2026-06-24 14:23:57 +0200

    chore: adjust navigation item title sizes

--- web_application/src/app/[lang]/admin/(secure)/components/Navigation/List.tsx
+++ web_application/src/app/[lang]/admin/(secure)/components/Navigation/List.tsx
@@ -34,7 +34,7 @@ export default function List({ items }: Props) {
                     if ('items' in item) {
                         return (
                             <li key={index}>
-                                <Text className="font py-2 leading-[1em] tracking-wide text-slate-800 uppercase">
+                                <Text className="font py-2 text-xs leading-[1em] tracking-wide text-slate-800 uppercase">
                                     {item.title}
                                 </Text>
                                 <ul className="flex-col md:flex">
