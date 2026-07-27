9503d864e8ffcae395c3f01715824213debbeb07
Fatemeh Amani <fatemeh.amani@visuellverstehen.de>
2026-07-27 15:29:00 +0200

    feat: show pointer cursor on hover state

--- web_application/src/app/components/ui/button.tsx
+++ web_application/src/app/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/shadcn';
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center gap-2 rounded-full font-medium not-italic tracking-[0.1px] transition-all disabled:pointer-events-none focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-borderFocus disabled:bg-transparent disabled:text-textDisabled',
+    'inline-flex items-center justify-center gap-2 rounded-full font-medium not-italic tracking-[0.1px] transition-all hover:cursor-pointer disabled:pointer-events-none focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-borderFocus disabled:bg-transparent disabled:text-textDisabled',
     {
         variants: {
             variant: {
